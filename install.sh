b5cf132d7cfdafd175946e0c79e648fcef9e0000
yuhui <suglow006@gmail.com>
2021-12-27 07:21:24 -0800

    fix pkg and change zinit lib

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if [ `so` = "linux" ]; then
     if [ -x "$(command -v pacman)" ]; then
         xargs -0 -n 1 sudo pacman -Syu --noconfirm < <(tr \\n \\0 <"$DOTFILES_FOLDER/pacman.pkglist")
     elif [ -x "$(command -v apt)" ]; then
-        xargs sudo apt-get install < "$DOTFILES_FOLDER/apt.pkglist"
+        xargs sudo apt-get -y install < "$DOTFILES_FOLDER/apt.pkglist"
     fi
 # todo snap
 #    if [ -x "$(command -v snap)" ]; then
