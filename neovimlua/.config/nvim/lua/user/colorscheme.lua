cedbfb1f73273b21ed0aeceb63a525cab86805ec
suglow006 <suglow006@gmail.com>
2022-01-04 23:19:42 +0800

    fix colorscheme gruvbox

--- neovimlua/.config/nvim/lua/user/colorscheme.lua
+++ neovimlua/.config/nvim/lua/user/colorscheme.lua
@@ -2,7 +2,16 @@ vim.cmd [[
 try
   set background=dark
   colorscheme darkplus 
-  colorscheme grovbox 
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
+
+vim.cmd [[
+try
+  set background=dark
+  colorscheme gruvbox 
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
