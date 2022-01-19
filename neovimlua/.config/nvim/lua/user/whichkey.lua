8d0d84197117e0b1c9507678dfd214fad16350c6
yuhui <y00380667@huawei.com>
2022-01-19 03:01:34 +0000

    fix whichkey

--- neovimlua/.config/nvim/lua/user/whichkey.lua
+++ neovimlua/.config/nvim/lua/user/whichkey.lua
@@ -95,7 +95,7 @@ local mappings = {
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
   ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
-
+  ["W"] = { "<cmd>Telescope grep_string<cr>", "Find word" },
   p = {
     name = "Packer",
     c = { "<cmd>PackerCompile<cr>", "Compile" },
