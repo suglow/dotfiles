b4498dae037ce37ebbcb2f7497ed2206e4fcabff
suglow006 <suglow006@gmail.com>
2022-04-26 22:41:17 +0800

    add fzf finder and update neovim to 0.7.0

--- neovimlua/.config/nvim/lua/user/plugins.lua
+++ neovimlua/.config/nvim/lua/user/plugins.lua
@@ -85,7 +85,7 @@ return packer.startup(function(use)
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
-
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
