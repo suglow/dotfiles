3cd97cfd1bf9e36d3a8330df536ac09290a753a3
suglow006 <suglow006@gmail.com>
2022-04-26 22:41:17 +0800

    add fzf finder and update neovim to 0.7.0

--- luanvim_install.sh
+++ luanvim_install.sh
@@ -26,7 +26,7 @@ if [ `so` = "linux" ]; then
         xargs -i sh -c "sudo apt-get -y install {} || true" < "$DOTFILES_FOLDER/apt.pkglist"
         mkdir -p ~/.local
         # install neovim 0.6.1
-        wget --no-check-certificate https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-linux64.tar.gz -O ~/.local/nvim-linux64.tar.gz
+        wget --no-check-certificate https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.tar.gz -O ~/.local/nvim-linux64.tar.gz
         tar -xvf ~/.local/nvim-linux64.tar.gz -C ~/.local
         sudo ln -s ~/.local/nvim-linux64/bin/nvim /usr/bin/nvim
     elif [ -x "$(command -v zypper)" ]; then
