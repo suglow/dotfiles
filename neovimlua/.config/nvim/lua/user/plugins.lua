d367c127fbc9de93a50e9af741b913bc244e8f75
suglow006 <suglow006@gmail.com>
2022-05-03 11:43:07 +0800

    add yank script

--- neovimlua/.config/nvim/lua/user/plugins.lua
+++ neovimlua/.config/nvim/lua/user/plugins.lua
@@ -59,6 +59,7 @@ return packer.startup(function(use)
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
   use "mfussenegger/nvim-dap"
+  use {'kevinhwang91/nvim-bqf', ft = 'qf'}
   -- Colorschemes
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   -- use "lunarvim/darkplus.nvim"
