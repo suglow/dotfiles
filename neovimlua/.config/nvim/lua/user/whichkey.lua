9526b9daa2d17e99c1bd308a130fbf354bc38d7d
yuhui <y00380667@huawei.com>
2022-05-20 17:19:11 +0800

    add hop and multi

--- neovimlua/.config/nvim/lua/user/whichkey.lua
+++ neovimlua/.config/nvim/lua/user/whichkey.lua
@@ -86,9 +86,9 @@ local mappings = {
   },
   ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
   ["w"] = { "<cmd>w!<CR>", "Save" },
-  ["q"] = { "<cmd>q!<CR>", "Quit" },
+  ["x"] = { "<cmd>q!<CR>", "Quit" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
-  ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
+  ["j"] = { "<cmd>HopChar1<CR>", "Hop Word" },
   ["f"] = {
     "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
     "Find files",
