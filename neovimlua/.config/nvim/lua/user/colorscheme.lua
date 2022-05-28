ae84609125489dbf858640d804957587e975adee
suglow006 <suglow006@gmail.com>
2022-05-28 15:08:46 +0800

    add git some plugins

--- neovimlua/.config/nvim/lua/user/colorscheme.lua
+++ neovimlua/.config/nvim/lua/user/colorscheme.lua
@@ -1,12 +1,12 @@
-vim.cmd [[
-try
-  set background=dark
-  colorscheme darkplus
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
+-- vim.cmd [[
+-- try
+--   set background=dark
+--   colorscheme darkplus
+-- catch /^Vim\%((\a\+)\)\=:E185/
+--   colorscheme default
+--   set background=dark
+-- endtry
+-- ]]
 
 vim.cmd [[
 try
