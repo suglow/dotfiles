e07476bc6355cd6e904f290ebbc8994a91ab7bca
suglow006 <suglow006@gmail.com>
2022-05-28 11:08:08 +0800

    add zen mode

--- neovimlua/.config/nvim/lua/user/plugins.lua
+++ neovimlua/.config/nvim/lua/user/plugins.lua
@@ -145,7 +145,8 @@ return packer.startup(function(use)
     end
   }
   
-
+  use { "nyngwang/NeoZoom.lua", branch = "neo-zoom-original" }
+  use "folke/zen-mode.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
