9903303f7d004da6fc8b62340ed55e80d2ba99a6
suglow006 <suglow006@gmail.com>
2022-06-07 21:04:56 +0800

    fix debug window

--- neovimlua/.config/nvim/lua/user/options.lua
+++ neovimlua/.config/nvim/lua/user/options.lua
@@ -58,7 +58,7 @@ if workspace ~= nil then
 end
 
 vim.g.windowswap_map_keys = 0
-vim.g.nvimgdb_termwin_command = "belowright vnew"
-vim.g.nvimgdb_codewin_command = "vnew"
+-- vim.g.nvimgdb_termwin_command = "belowright vnew"
+-- vim.g.nvimgdb_codewin_command = "vnew"
 vim.g.nvimgdb_use_find_executables = 0
 vim.g.nvimgdb_use_cmake_to_find_executables = 0
