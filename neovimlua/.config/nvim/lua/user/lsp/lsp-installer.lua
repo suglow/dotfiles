7108172af5068b88bb9593e2c876fd5246d069ef
suglow006 <suglow006@gmail.com>
2022-06-14 06:53:29 +0000

    fix lsp setup

--- neovimlua/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ neovimlua/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -1,8 +1,3 @@
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not status_ok then
-	return
-end
-
 local servers = {
   "sumneko_lua",
   "pyright",
@@ -12,33 +7,6 @@ local servers = {
 }
 
 local settings = {
-  ensure_installed = servers,
-  -- automatic_installation = false,
-  ui = {
-    icons = {
-      -- server_installed = "◍",
-      -- server_pending = "◍",
-      -- server_uninstalled = "◍",
-      -- server_installed = "✓",
-      -- server_pending = "➜",
-      -- server_uninstalled = "✗",
-    },
-    keymaps = {
-      toggle_server_expand = "<CR>",
-      install_server = "i",
-      update_server = "u",
-      check_server_version = "c",
-      update_all_servers = "U",
-      check_outdated_servers = "C",
-      uninstall_server = "X",
-    },
-  },
-
-  log_level = vim.log.levels.INFO,
-  -- max_concurrent_installers = 4,
-  -- install_root_dir = path.concat { vim.fn.stdpath "data", "lsp_servers" },
-}
-lsp_installer.setup(settings)
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
