fd555a01aa9d17696f2abf3d0643cac7eba380c6
suglow006 <suglow006@gmail.com>
2022-06-15 06:42:58 +0000

    lsp installer default lang server

--- neovimlua/.config/nvim/lua/user/plugins.lua
+++ neovimlua/.config/nvim/lua/user/plugins.lua
@@ -93,9 +93,9 @@ return packer.startup(function(use)
         local servers = {
           "sumneko_lua",
           "pyright",
-          "yamlls",
+          -- "yamlls",
           "bashls",
-          "clangd",
+          -- "clangd",
         }
         local settings = {
           ensure_installed = servers,
