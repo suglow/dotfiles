fca0f343bad7c74b1e6230724db26d76e86d5db5
suglow006 <suglow006@gmail.com>
2022-06-15 15:28:32 +0800

    get HOME envvar

--- neovimlua/.config/nvim/lua/user/dap/codelldb.lua
+++ neovimlua/.config/nvim/lua/user/dap/codelldb.lua
@@ -1,5 +1,5 @@
 
-local extension_path = "/home/suglow/.local/bin/codelldb/extension/" 
+local extension_path = os.getenv( "HOME" ) .. "/.local/bin/codelldb/extension/" 
 local codelldb_path = extension_path .. 'adapter/codelldb'
 local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
 
