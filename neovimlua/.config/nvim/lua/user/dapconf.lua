f94d8c17efeec3754546ed50b9cbff2255f59700
suglow006 <suglow006@gmail.com>
2022-06-15 13:09:46 +0800

    rust code dap switch to codelldb

--- neovimlua/.config/nvim/lua/user/dapconf.lua
+++ neovimlua/.config/nvim/lua/user/dapconf.lua
@@ -15,49 +15,50 @@ if not dap_ui_status_ok then
 end
 dap_virt.setup()
 
-dapui.setup {
-  icons = { expanded = "▾", collapsed = "▸" },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  sidebar = {
-    -- You can change the order of elements in the sidebar
-    elements = {
-      -- Provide as ID strings or tables with "id" and "size" keys
-      {
-        id = "scopes",
-        size = 0.25, -- Can be float or integer > 1
-      },
-      { id = "breakpoints", size = 0.25 },
-      -- { id = "stacks", size = 0.25 },
-      -- { id = "watches", size = 00.25 },
-    },
-    size = 40,
-    position = "right", -- Can be "left", "right", "top", "bottom"
-  },
-  tray = {
-    elements = {},
-    -- elements = { "repl" },
-    -- size = 10,
-    -- position = "bottom", -- Can be "left", "right", "top", "bottom"
-  },
-  floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
-    border = "rounded", -- Border style. Can be "single", "double" or "rounded"
-    mappings = {
-      close = { "q", "<Esc>" },
-    },
-  },
-  windows = { indent = 1 },
-}
+dapui.setup()
 
+-- dapui.setup {
+--   icons = { expanded = "▾", collapsed = "▸" },
+--   mappings = {
+--     -- Use a table to apply multiple mappings
+--     expand = { "<CR>", "<2-LeftMouse>" },
+--     open = "o",
+--     remove = "d",
+--     edit = "e",
+--     repl = "r",
+--     toggle = "t",
+--   },
+--   sidebar = {
+--     -- You can change the order of elements in the sidebar
+--     elements = {
+--       -- Provide as ID strings or tables with "id" and "size" keys
+--       {
+--         id = "scopes",
+--         size = 0.25, -- Can be float or integer > 1
+--       },
+--       { id = "breakpoints", size = 0.25 },
+--       -- { id = "stacks", size = 0.25 },
+--       -- { id = "watches", size = 00.25 },
+--     },
+--     size = 40,
+--     position = "right", -- Can be "left", "right", "top", "bottom"
+--   },
+--   tray = {
+--     elements = {},
+--     -- elements = { "repl" },
+--     -- size = 10,
+--     -- position = "bottom", -- Can be "left", "right", "top", "bottom"
+--   },
+--   floating = {
+--     max_height = nil, -- These can be integers or a float between 0 and 1.
+--     max_width = nil, -- Floats will be treated as percentage of your screen.
+--     border = "rounded", -- Border style. Can be "single", "double" or "rounded"
+--     mappings = {
+--       close = { "q", "<Esc>" },
+--     },
+--   },
+--   windows = { indent = 1 },
+-- }
 local icons = require "user.icons"
 
 vim.fn.sign_define('DapBreakpoint', {text=icons.ui.Bug, texthl='DiagnosticSignError', linehl='', numhl=''})
@@ -83,9 +84,12 @@ for dap_name, dap_options in pairs(dap_config) do
 end
 
 adapter_lldb = require("user.dap.cpprust").adapters
+adapter_codelldb = require("user.dap.codelldb").adapters
 config_lldb = require("user.dap.cpprust").configurations
+config_codelldb = require("user.dap.cpprust").configurations
 dap.adapters.lldb = adapter_lldb
-dap.configurations.rust = config_lldb
+dap.adapters.codelldb = adapter_codelldb
+dap.configurations.rust = config_codelldb 
 dap.configurations.c = config_lldb
 dap.configurations.cpp = config_lldb
 
