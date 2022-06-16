4d71c16ce6750188e7cb1dd87c302a28215c6598
suglow006 <suglow006@gmail.com>
2022-06-16 10:25:32 +0000

    fix lsp install config

--- neovimlua/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ neovimlua/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -29,10 +29,10 @@ for _, server in pairs(servers) do
     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
   end
 
-  if server == "pyright" then
-    local pyright_opts = require "user.lsp.settings.pyright"
-    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-  end
+--  if server == "pyright" then
+--    local pyright_opts = require "user.lsp.settings.pyright"
+--    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+--  end
 
   lspconfig[server].setup(opts)
 end
