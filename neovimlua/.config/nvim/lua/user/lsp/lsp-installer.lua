005832fe99103f48b5121c49edbbf333606004f9
suglow006 <suglow006@gmail.com>
2022-07-15 02:24:36 +0000

    rusttools lsp init by lsp lsp-installer

--- neovimlua/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ neovimlua/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -3,6 +3,22 @@ if not status_ok then
   return
 end
 
+local hdl_status_ok, handlers = pcall(require, "user.lsp.handlers")
+if not hdl_status_ok then
+  return
+end
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
+local rust_status_ok, rusttools = pcall(require, "rust-tools")
+if not rust_status_ok then
+  return
+end
+
+
 local servers = {
   -- "cssls",
   -- "cssmodules_ls",
@@ -11,6 +27,7 @@ local servers = {
   -- "jdtls",
   --  "jsonls",
   -- "solc",
+  "rust-analyzer",
   "sumneko_lua",
   -- "tflint",
   -- "tsserver",
@@ -50,33 +67,57 @@ local settings = {
 
 lsp_installer.setup(settings)
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-  return
-end
 
-local opts = {}
+local extension_path = os.getenv("HOME") .. "/.local/bin/codelldb/extension/"
+local codelldb_path = extension_path .. 'adapter/codelldb'
+local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
+
+local opts_rust = {
+  server = {
+    -- standalone file support
+    -- setting it to false may improve startup time
+    -- standalone = true,
+    ["rust-analyzer"] = {
+      -- enable clippy on save
+      checkOnSave = {
+        command = "clippy"
+      }
+    },
+    capabilities = handlers.capabilities,
+    on_attach = handlers.on_attach,
+  }, -- rust-analyzer options
+  dap = {
+    adapter = require('rust-tools.dap').get_codelldb_adapter(
+      codelldb_path, liblldb_path),
+  }
+}
 
 for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
+  local opts = {
+    on_attach = handlers.on_attach,
+    capabilities = handlers.capabilities,
   }
 
-if server == "jsonls" then
-  local jsonls_opts = require "user.lsp.settings.jsonls"
-  opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-end
+  if server == "jsonls" then
+    local jsonls_opts = require "user.lsp.settings.jsonls"
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  end
 
-if server == "sumneko_lua" then
-  local sumneko_opts = require "user.lsp.settings.sumneko_lua"
-  opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-end
+  if server == "sumneko_lua" then
+    local sumneko_opts = require "user.lsp.settings.sumneko_lua"
+    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  end
 
-if server == "pyright" then
-  local pyright_opts = require "user.lsp.settings.pyright"
-  opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-end
+  if server == "pyright" then
+    local pyright_opts = require "user.lsp.settings.pyright"
+    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+  end
+
+  if server == "rust-analyzer" then
+    rusttools.setup(opts_rust)
+    goto continue
+  end
+
 
   -- if server == "solang" then
   --   local solang_opts = require "user.lsp.settings.solang"
@@ -94,4 +135,5 @@ end
   -- end
 
   lspconfig[server].setup(opts)
+  ::continue::
 end
