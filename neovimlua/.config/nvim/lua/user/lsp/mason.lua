f89e51f94f64bf58f7a969c6a6b48682aa18ed8d
suglow006 <suglow006@gmail.com>
2022-07-30 07:16:01 +0000

    update

--- neovimlua/.config/nvim/lua/user/lsp/mason.lua
+++ neovimlua/.config/nvim/lua/user/lsp/mason.lua
@@ -19,71 +19,71 @@ local servers = {
   "gopls",
 }
 
-local settings = {
-  ui = {
-    border = "rounded",
-    icons = {
-      package_installed = "◍",
-      package_pending = "◍",
-      package_uninstalled = "◍",
-    },
-  },
-  log_level = vim.log.levels.INFO,
-  max_concurrent_installers = 4,
-}
-
-mason.setup(settings)
-mason_lspconfig.setup {
-  ensure_installed = servers,
-  automatic_installation = true,
-}
+-- local settings = {
+--   ui = {
+--     border = "rounded",
+--     icons = {
+--       package_installed = "◍",
+--       package_pending = "◍",
+--       package_uninstalled = "◍",
+--     },
+--   },
+--   log_level = vim.log.levels.INFO,
+--   max_concurrent_installers = 4,
+-- }
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
   return
 end
+-- mason.setup(settings)
+mason.setup({})
 
-local opts = {}
+local common_opts = {
+  on_attach = require("user.lsp.handlers").on_attach,
+  capabilities = require("user.lsp.handlers").capabilities,
+}
 
-for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
-  }
-
-  server = vim.split(server, "@")[1]
-
-  if server == "jsonls" then
+mason_lspconfig.setup {
+  ensure_installed = servers,
+  automatic_installation = true,
+}
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    lspconfig[server_name].setup {}
+  end,
+  ["jsonls"] = function()
     local jsonls_opts = require "user.lsp.settings.jsonls"
-    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-  end
-
-  if server == "sumneko_lua" then
-    local l_status_ok, lua_dev = pcall(require, "lua-dev")
-    if not l_status_ok then
-      return
-    end
-    -- local sumneko_opts = require "user.lsp.settings.sumneko_lua"
-    -- opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-    -- opts = vim.tbl_deep_extend("force", require("lua-dev").setup(), opts)
+    local opts = vim.tbl_deep_extend("force", jsonls_opts, common_opts)
+    lspconfig["jsonls"].setup(opts)
+  end,
+  ["sumneko_lua"] = function()
+    local _, lua_dev = pcall(require, "lua-dev")
     local luadev = lua_dev.setup {
-      --   -- add any options here, or leave empty to use the default settings
-      -- lspconfig = opts,
       lspconfig = {
-        on_attach = opts.on_attach,
-        capabilities = opts.capabilities,
+        on_attach = common_opts.on_attach,
+        capabilities = common_opts.capabilities,
         --   -- settings = opts.settings,
       },
     }
-    lspconfig.sumneko_lua.setup(luadev)
-    goto continue
-  end
-
-
-  if server == "pyright" then
+    lspconfig["sumneko_lua"].setup(luadev)
+  end,
+  ["pyright"] = function()
     local pyright_opts = require "user.lsp.settings.pyright"
-    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+    local opts = vim.tbl_deep_extend("force", pyright_opts, common_opts)
+    lspconfig["pyright"].setup(opts)
+  end,
+  ["rust_analyzer"] = function()
+    local rust_opts = require "user.lsp.settings.rust"
+    local rust_tools_status_ok, rust_tools = pcall(require, "rust-tools")
+    if not rust_tools_status_ok then
+      return
+    end
+    rust_tools.setup(rust_opts)
   end
+}
+
+
 
 --  if server == "gopls" then
 --    local path = require 'mason.core.path'
@@ -119,19 +119,3 @@ for _, server in pairs(servers) do
 --    })
 --    goto continue
 --  end
-
-  if server == "rust_analyzer" then
-    local rust_opts = require "user.lsp.settings.rust"
-    -- opts = vim.tbl_deep_extend("force", rust_opts, opts)
-    local rust_tools_status_ok, rust_tools = pcall(require, "rust-tools")
-    if not rust_tools_status_ok then
-      return
-    end
-
-    rust_tools.setup(rust_opts)
-    goto continue
-  end
-
-  lspconfig[server].setup(opts)
-  ::continue::
-end
