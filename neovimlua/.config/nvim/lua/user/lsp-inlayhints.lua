3d4e75ab5ee61551a479739f063714791536fa21
suglow006 <suglow006@gmail.com>
2022-08-27 01:22:37 +0000

    lsp inlayhints check neovim version

--- neovimlua/.config/nvim/lua/user/lsp-inlayhints.lua
+++ neovimlua/.config/nvim/lua/user/lsp-inlayhints.lua
@@ -4,17 +4,20 @@ if not status_ok then
 end
 
 local group = vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = "LspAttach_inlayhints",
-  callback = function(args)
-    if not (args.data and args.data.client_id) then
-      return
-    end
 
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    require("lsp-inlayhints").on_attach(args.buf, client)
-  end,
-})
+if vim.fn.has("nvim-0.8") == 1 then
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = "LspAttach_inlayhints",
+    callback = function(args)
+      if not (args.data and args.data.client_id) then
+        return
+      end
+
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      require("lsp-inlayhints").on_attach(args.buf, client)
+    end,
+  })
+end
 
 hints.setup {
   inlay_hints = {
