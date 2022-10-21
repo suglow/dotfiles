6d73bc8d3568b316068c44ee6fd7148ac1ebc6e1
suglow006 <suglow006@gmail.com>
2022-10-21 14:50:02 +0800

    add telescope history config

--- neovimlua/.config/nvim/lua/user/telescope.lua
+++ neovimlua/.config/nvim/lua/user/telescope.lua
@@ -35,6 +35,8 @@ telescope.setup {
         ["<C-n>"] = actions.cycle_history_next,
         ["<C-p>"] = actions.cycle_history_prev,
 
+        ["<C-Down>"] = require('telescope.actions').cycle_history_next,
+        ["<C-Up>"] = require('telescope.actions').cycle_history_prev,
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
 
