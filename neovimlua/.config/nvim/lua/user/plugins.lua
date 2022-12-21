a41ed4391ffb0c580f79212bd2a39fd7c0af4abc
suglow006 <suglow006@gmail.com>
2022-12-21 11:00:15 +0000

    add emment

--- neovimlua/.config/nvim/lua/user/plugins.lua
+++ neovimlua/.config/nvim/lua/user/plugins.lua
@@ -58,6 +58,7 @@ return packer.startup(function(use)
   use "goolord/alpha-nvim"
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
+  use "mattn/emmet-vim"
   -- use { "axkirillov/telescope-changed-files" }
   -- DAP
   use("mfussenegger/nvim-dap")
