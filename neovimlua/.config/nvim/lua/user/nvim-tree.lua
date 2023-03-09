1843830eadebfba36cf906cb82f7bd75ed0f9552
suglow006 <suglow006@gmail.com>
2023-03-09 13:01:34 +0000

    fix nvim-tree key maps

--- neovimlua/.config/nvim/lua/user/nvim-tree.lua
+++ neovimlua/.config/nvim/lua/user/nvim-tree.lua
@@ -14,10 +14,47 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 local function custom_callback(callback_name)
-    return string.format(":lua require('user.treeutils').%s()<CR>", callback_name)
+  return string.format("<cmd>lua require('user.treeutils').%s()<CR>", callback_name)
 end
 
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- Mappings migrated from view.mappings.list
+  --
+  -- You will need to insert "your code goes here" for any mappings with a custom action_cb
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+
+  --[[ { key = "<c-f>", cb = custom_callback "launch_find_files" }, ]]
+  --[[ { key = "<c-g>", cb = custom_callback "launch_live_grep" }, ]]
+  --[[ { key = "<c-d>", cb = custom_callback "launch_live_grep_args" }, ]]
+  --[[ { key = "T", cb = custom_callback "toggle_term" }, ]]
+  --[[ { key = "<c-t>", cb = custom_callback "toggle_term" }, ]]
+  vim.keymap.set('n', '<c-f>', function()
+    require('user.treeutils').launch_find_files()
+  end, opts('Find files'))
+  vim.keymap.set('n', '<c-g>', function()
+    require('user.treeutils').launch_live_grep()
+  end, opts('Live Grep'))
+  vim.keymap.set('n', '<c-d>', function()
+    require('user.treeutils').launch_live_grep_args()
+  end, opts('Live Grep Directry'))
+  vim.keymap.set('n', '<c-t>', function()
+    require('user.treeutils').toggle_term()
+  end, opts('Toggle Term'))
+end
+
+
 nvim_tree.setup {
+  on_attach = on_attach,
   disable_netrw = true,
   hijack_netrw = true,
   open_on_tab = false,
@@ -87,12 +124,12 @@ nvim_tree.setup {
       list = {
         { key = { "<CR>", "o" }, cb = tree_cb "edit" },
         -- { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-        { key = "<c-f>", cb = custom_callback "launch_find_files" },
-        { key = "<c-g>", cb = custom_callback "launch_live_grep" },
-        { key = "<c-d>", cb = custom_callback "launch_live_grep_args" },
-        { key = "T", cb = custom_callback "toggle_term" },
-        { key = "<c-t>", cb = custom_callback "toggle_term" },
+        { key = "v",             cb = tree_cb "vsplit" },
+        { key = "<c-f>",         cb = custom_callback "launch_find_files" },
+        { key = "<c-g>",         cb = custom_callback "launch_live_grep" },
+        { key = "<c-d>",         cb = custom_callback "launch_live_grep_args" },
+        { key = "T",             cb = custom_callback "toggle_term" },
+        { key = "<c-t>",         cb = custom_callback "toggle_term" },
       },
     },
     number = false,
