63a8fbf01ee2e8bfc48ea774ebdef4a4b596c5bb
suglow006 <suglow006@gmail.com>
2023-04-21 08:53:05 +0000

    fix clangd error

--- neovimlua/.config/nvim/lua/user/lsp/handlers.lua
+++ neovimlua/.config/nvim/lua/user/lsp/handlers.lua
@@ -8,6 +8,7 @@ if not status_cmp_ok then
 end
 M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
+M.capabilities.offsetEncoding = {'utf-16'}
 
 M.setup = function()
   local icons = require "user.icons"
