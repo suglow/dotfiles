a62bd4a42cb3b2e76dc036814664791150717bce
suglow006 <suglow006@gmail.com>
2023-05-11 12:53:08 +0000

    config lazyvim neo-tree and telescope and keymap

--- lazynv/.lazynv/.config/nvim/lua/config/keymaps.lua
+++ lazynv/.lazynv/.config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,15 @@ vim.keymap.del("n", "<leader><tab><tab>")
 vim.keymap.del("n", "<leader><tab>]")
 vim.keymap.del("n", "<leader><tab>d")
 vim.keymap.del("n", "<leader><tab>[")
+
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==", { noremap = true, desc = "window move right" })
+vim.keymap.set("v", "<A-k>", ":m .-2<CR>==", { noremap = true, desc = "window move left" })
+
+vim.keymap.set("n", "<leader>r", "viwP", { noremap = true, desc = "repace world" })
+-- Visual Block --
+-- Move text up and down
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", { noremap = true, desc = "move line down" })
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", { noremap = true, desc = "move line up" })
+
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv", { noremap = true, desc = "move select down" })
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv", { noremap = true, desc = "move select up" })
