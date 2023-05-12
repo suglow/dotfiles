7bb699c340e2e730d5ec9d6f7ea67f1c55b3df23
suglow006 <suglow006@gmail.com>
2023-05-12 07:11:06 +0000

    add key maps for unimpaired

--- lazynv/.lazynv/.config/nvim/lua/config/options.lua
+++ lazynv/.lazynv/.config/nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+opt.relativenumber = false -- Relative line numbers
+-- vim.cmd "set listchars=tab:→\\ ,eol:↲,nbsp:␣,space:•"
+opt.listchars = {
+  eol = "↲",
+  space = "•",
+  tab = "→\\ ",
+  nbsp = "␣",
+}
+vim.opt.list = false
