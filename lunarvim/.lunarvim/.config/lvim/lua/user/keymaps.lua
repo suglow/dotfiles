30ebc4c1229c1243440e8142bc41e2eec3d3bb3e
suglow006 <suglow006@gmail.com>
2023-05-15 12:11:12 +0000

    add yo leader key maps

--- lunarvim/.lunarvim/.config/lvim/lua/user/keymaps.lua
+++ lunarvim/.lunarvim/.config/lvim/lua/user/keymaps.lua
@@ -24,5 +24,19 @@ lvim.keys.normal_mode["<A-l>"] = ":vertical resize +2<CR>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
-
 lvim.builtin.which_key.mappings["<tab>"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" }
+
+lvim.keys.normal_mode["<A-j>"] = ":resize -2<CR>"
+lvim.keys.normal_mode["<A-k>"] = ":resize +2<CR>"
+lvim.keys.normal_mode["<A-h>"] = ":vertical resize -2<CR>"
+lvim.keys.normal_mode["<A-l>"] = ":vertical resize +2<CR>"
+lvim.keys.normal_mode["yow"] = ":set wrap!<cr>"
+lvim.keys.normal_mode["yoc"] = ":set cursorline!<cr>"
+lvim.keys.normal_mode["yoh"] = ":set hlsearch!<cr>"
+lvim.keys.normal_mode["yoi"] = ":set ignorecase!<cr>"
+lvim.keys.normal_mode["yol"] = ":set list!<cr>"
+lvim.keys.normal_mode["yon"] = ":set number!<cr>"
+lvim.keys.normal_mode["yor"] = ":set relativenumber!<cr>"
+lvim.keys.normal_mode["yos"] = ":set spell!<cr>"
+lvim.keys.normal_mode["you"] = ":set cursorcolumn!<cr>"
+lvim.keys.normal_mode["yov"] = ":set virtualedit!<cr>"
