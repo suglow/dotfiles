ade0cbb8cdb9a098fd6949e9155fbe9329e23423
suglow006 <suglow006@gmail.com>
2023-05-15 07:19:24 +0000

    fix negivater to file

--- lazynv/.lazynv/.config/nvim/lua/plugins/neo-tree.lua
+++ lazynv/.lazynv/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,4 +1,34 @@
 local Util = require("lazyvim.util")
+local function getGrepTelescopeOpts(state, path)
+  return {
+    cwd = path,
+    search_dirs = { path },
+    attach_mappings = function(prompt_bufnr, map)
+      local actions = require("telescope.actions")
+      local fs = require("neo-tree.sources.filesystem.commands")
+      local utils = require("neo-tree.utils")
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local action_state = require("telescope.actions.state")
+        local selection = action_state.get_selected_entry()
+        local filename = selection.filename
+        if filename == nil then
+          filename = selection[1]
+        end
+        vim.notify(filename)
+        local row = selection.row or selection.lnum
+        local col = selection.col
+        -- any way to open the file without triggering auto-close event of neo-tree?
+        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+
+        utils.open_file(state, filename)
+        vim.api.nvim_win_set_cursor(0, { row, col })
+      end)
+      return true
+    end,
+  }
+end
+
 local function getTelescopeOpts(state, path)
   return {
     cwd = path,
@@ -59,12 +89,12 @@ return {
         telescope_grep = function(state)
           local node = state.tree:get_node()
           local path = node:get_id()
-          require("telescope.builtin").live_grep(getTelescopeOpts(state, path))
+          require("telescope.builtin").live_grep(getGrepTelescopeOpts(state, path))
         end,
         telescope_grep_args = function(state)
           local node = state.tree:get_node()
           local path = node:get_id()
-          require("telescope").extensions.live_grep_args.live_grep_args(getTelescopeOpts(state, path))
+          require("telescope").extensions.live_grep_args.live_grep_args(getGrepTelescopeOpts(state, path))
         end,
         telescope_find_file = function(state)
           local path = require("lazyvim.util").get_root()
