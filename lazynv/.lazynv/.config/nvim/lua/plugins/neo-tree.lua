185a06c23b46de4f6c2b58b37d7ce54b1cdb688e
suglow006 <suglow006@gmail.com>
2023-05-15 08:20:30 +0000

    fix negivater to file

--- lazynv/.lazynv/.config/nvim/lua/plugins/neo-tree.lua
+++ lazynv/.lazynv/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,7 +6,6 @@ local function getGrepTelescopeOpts(state, path)
     attach_mappings = function(prompt_bufnr, map)
       local actions = require("telescope.actions")
       local fs = require("neo-tree.sources.filesystem.commands")
-      local utils = require("neo-tree.utils")
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local action_state = require("telescope.actions.state")
@@ -15,7 +14,7 @@ local function getGrepTelescopeOpts(state, path)
         if filename == nil then
           filename = selection[1]
         end
-        vim.notify(filename)
+        -- vim.notify(filename)
         local row = selection.row or selection.lnum
         local col = selection.col
         -- any way to open the file without triggering auto-close event of neo-tree?
@@ -35,6 +34,7 @@ local function getTelescopeOpts(state, path)
     search_dirs = { path },
     attach_mappings = function(prompt_bufnr, map)
       local actions = require("telescope.actions")
+      local utils = require("neo-tree.utils")
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local action_state = require("telescope.actions.state")
@@ -45,6 +45,7 @@ local function getTelescopeOpts(state, path)
         end
         -- any way to open the file without triggering auto-close event of neo-tree?
         require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+        utils.open_file(state, filename)
       end)
       return true
     end,
