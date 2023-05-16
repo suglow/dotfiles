8104951f8cdb53409bc12ac8515e528b0fe964ec
suglow006 <suglow006@gmail.com>
2023-05-16 04:14:54 +0000

    make notice better

---
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/notice.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- cmdline = {
+      -- view = "cmdline",
+      -- },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 5,
+            col = "50%",
+          },
+          size = {
+            width = 150,
+            height = "auto",
+          },
+        },
+      },
+    },
+  },
+}
