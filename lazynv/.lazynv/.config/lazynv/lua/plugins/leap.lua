93ac3d04c0813db4bf6a1748665162463375387c
suglow006 <suglow006@gmail.com>
2023-06-12 18:52:07 +0800

    leap config change and add nvim-surround

--- lazynv/.lazynv/.config/lazynv/lua/plugins/leap.lua
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/leap.lua
@@ -46,5 +46,15 @@ return {
         "z",
       },
     },
+    keys = nil,
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.setup(opts)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
+    end,
   },
 }
