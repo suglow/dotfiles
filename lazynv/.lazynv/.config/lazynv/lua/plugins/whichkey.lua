6f6cd3e4020b8c1a53e24f0111d47b4709ce64b7
suglow006 <suglow006@gmail.com>
2023-06-17 13:13:01 +0000

    add lsp keymaps

--- lazynv/.lazynv/.config/lazynv/lua/plugins/whichkey.lua
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/whichkey.lua
@@ -5,7 +5,6 @@ return {
       defaults = {
         ["<leader><tab>"] = { "<leader>fE", "Explorer NeoTree (cwd)", remap = true },
         ["<leader>a"] = { "<cmd>Alpha<cr>", "Alpha" },
-        ["<leader>l"] = vim.NIL,
         ["<leader>p"] = { "<cmd>Lazy<cr>", "Lazy" },
         ["<leader>sj"] = { "<cmd>Telescope jumplist<cr>", "Jump list" },
         ["yo"] = {
@@ -19,6 +18,32 @@ return {
           s = { "<cmd>set spell!<cr>", "Toggle spell" },
           u = { "<cmd>set cursorcolumn!<cr>", "Toggle cursorcolumn" },
           v = { "<cmd>set virtualedit!<cr>", "Toggle virtualedit" },
+        },
+        ["<leader>l"] = {
+          name = "LSP",
+          a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+          d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+          w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+          f = { "<cmd>lua require('lvim.lsp.utils').format()<cr>", "Format" },
+          i = { "<cmd>LspInfo<cr>", "Info" },
+          I = { "<cmd>Mason<cr>", "Mason Info" },
+          j = {
+            "<cmd>lua vim.diagnostic.goto_next()<cr>",
+            "Next Diagnostic",
+          },
+          k = {
+            "<cmd>lua vim.diagnostic.goto_prev()<cr>",
+            "Prev Diagnostic",
+          },
+          l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+          q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+          r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+          s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+          S = {
+            "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+            "Workspace Symbols",
+          },
+          e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
         },
       },
     },
