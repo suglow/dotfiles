258d72ca32ac5ce6ab91069acde2777c86f7b74f
suglow006 <suglow006@gmail.com>
2023-07-04 02:22:13 +0000

    feat(clangd): add office clangd config

--- lazynv/.lazynv/.config/lazynv/lua/config/lazy.lua
+++ lazynv/.lazynv/.config/lazynv/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
