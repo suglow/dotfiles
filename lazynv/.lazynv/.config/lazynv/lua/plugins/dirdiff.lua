d11fbfee65c576d1ff907ddeb91ee9f229725fe8
suglow006 <suglow006@gmail.com>
2023-09-18 02:41:12 +0000

    feat(diff): add hidden files

--- lazynv/.lazynv/.config/lazynv/lua/plugins/dirdiff.lua
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/dirdiff.lua
@@ -2,5 +2,8 @@ return {
   {
     "ZSaberLv0/ZFVimJob",
     dependencies = { "ZSaberLv0/ZFVimDirDiff" },
+    init = function()
+      vim.g.ZFIgnoreOption_ZFDirDiff = { bin = 0, media = 0, ZFDirDiff = 1, hidden = 1 }
+    end,
   },
 }
