7b1e55c4cd283168a2f7b522f87de492c3ce184e
suglow006 <suglow006@gmail.com>
2023-09-19 02:16:01 +0000

    feat(fix): add zfdirdiff depend

--- lazynv/.lazynv/.config/lazynv/lua/plugins/dirdiff.lua
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/dirdiff.lua
@@ -1,7 +1,7 @@
 return {
   {
     "ZSaberLv0/ZFVimJob",
-    dependencies = { "ZSaberLv0/ZFVimDirDiff" },
+    dependencies = { "ZSaberLv0/ZFVimDirDiff","ZSaberLv0/ZFVimIgnore" },
     init = function()
       vim.g.ZFIgnoreOption_ZFDirDiff = { bin = 0, media = 0, ZFDirDiff = 1, hidden = 1 }
     end,
