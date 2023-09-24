bec49e33d9ce97e7e95155596f755bef0f7a0fb8
suglow006 <suglow006@gmail.com>
2023-09-24 13:47:30 +0000

    fix(plugins): fix expand affact by .gitignore
    fix

--- lazynv/.lazynv/.config/lazynv/lua/plugins/dirdiff.lua
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/dirdiff.lua
@@ -1,9 +1,13 @@
 return {
   {
     "ZSaberLv0/ZFVimJob",
-    dependencies = { "ZSaberLv0/ZFVimDirDiff","ZSaberLv0/ZFVimIgnore" },
+    dependencies = { "ZSaberLv0/ZFVimDirDiff"},
     init = function()
-      vim.g.ZFIgnoreOption_ZFDirDiff = { bin = 0, media = 0, ZFDirDiff = 1, hidden = 1 }
+      -- vim.g.ZFIgnoreOption_ZFDirDiff = { bin = 1, media = 1, ZFDirDiff = 1, hidden = 0, common = 0, gitignore = 0 }
+      vim.g.ZFDirDiff_excludeCheck_fallback_patterns = {
+                   "^\\~.*$",
+                   "^.*\\~$",
+      }
     end,
   },
 }
