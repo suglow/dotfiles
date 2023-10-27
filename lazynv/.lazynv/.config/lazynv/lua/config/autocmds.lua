ef6371d7eda44eec3531572e779052147e4700fb
suglow006 <suglow006@gmail.com>
2023-10-27 09:10:31 +0000

    feat(comment): new line remove comment

--- lazynv/.lazynv/.config/lazynv/lua/config/autocmds.lua
+++ lazynv/.lazynv/.config/lazynv/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.opt.formatoptions:remove { "c", "r", "o" }
+  end,
+  desc = "Disable New Line Comment",
+})
