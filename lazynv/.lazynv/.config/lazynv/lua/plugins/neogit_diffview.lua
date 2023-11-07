aff96b4ea4de462a6e34738ebbd61eb2b072030e
suglow006 <suglow006@gmail.com>
2023-11-07 02:00:31 +0000

    fix(diffview): diff highlight optmase

--- lazynv/.lazynv/.config/lazynv/lua/plugins/neogit_diffview.lua
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/neogit_diffview.lua
@@ -1,5 +1,26 @@
 return {
-  { "sindrets/diffview.nvim", dependencies = "nvim-lua/plenary.nvim" },
+  {
+    "sindrets/diffview.nvim",
+    opts = {
+      hooks = {
+        diff_buf_win_enter = function(bufnr, winid, ctx)
+          if ctx.layout_name:match("^diff2") then
+            if ctx.symbol == "a" then
+              vim.opt_local.winhl = table.concat({
+                "DiffAdd:DiffviewDiffAddAsDelete",
+                "DiffDelete:DiffviewDiffDelete",
+              }, ",")
+            elseif ctx.symbol == "b" then
+              vim.opt_local.winhl = table.concat({
+                "DiffDelete:DiffviewDiffDelete",
+              }, ",")
+            end
+          end
+        end,
+      },
+    },
+    dependencies = "nvim-lua/plenary.nvim",
+  },
   {
     "NeogitOrg/neogit",
     opts = {
