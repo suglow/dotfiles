a56a59e7ab57d5af9ca0f594c16a86e6a1ec4073
suglow006 <suglow006@gmail.com>
2023-11-11 01:39:33 +0000

    fix(neorg): change workspace

--- lazynv/.lazynv/.config/lazynv/lua/plugins/flash.lua
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/flash.lua
@@ -5,7 +5,7 @@ return {
     vscode = true,
     ---@type Flash.Config
     opts = {
-      labels="asfghjklqwertuiozcvbnm",
+      labels="asfghjklqwertuiozvbnm",
       modes = {
         char = {
           jump_labels = true,
@@ -18,7 +18,8 @@ return {
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      -- { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "S", "vs", desc = "fast visual selection", remap = true},
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
