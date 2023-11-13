0d3883c5257c5c94471fb6c7a809ebff14354ca7
suglow006 <suglow006@gmail.com>
2023-11-13 06:07:31 +0000

    fix(lsp_line): lsp_line fix

--- lazynv/.lazynv/.config/lazynv/lua/plugins/lsp_lines.lua
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/lsp_lines.lua
@@ -1,11 +1,9 @@
 return {
   {
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    event = "LspAttach",
     config = function()
       require("lsp_lines").setup()
-      -- https://github.com/folke/lazy.nvim/issues/620
-      -- vim.diagnostic.config({ virtual_text = false, }, require("lazy.core.config").ns)
-      -- vim.diagnostic.config({ virtual_lines = { prefix = "" } })
       vim.diagnostic.config({
         update_in_insert = false,
         signs = false,
@@ -13,10 +11,9 @@ return {
         virtual_text = false, -- Since we're using lsp_lines
         virtual_lines = false,
       }, require("lazy.core.config").ns)
-      vim.diagnostic.config({ virtual_lines = { prefix = "" } })
     end,
     keys = {
       { "<leader>ln", require("lsp_lines").toggle, desc = "Toggle lsp_lines" },
-    }
+    },
   },
 }
