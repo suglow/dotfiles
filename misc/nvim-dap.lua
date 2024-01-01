5539cd93b56ca191d2a915dfe294e033ea628f52
suglow006 <suglow006@gmail.com>
2024-01-01 20:02:15 +0800

    add dap config

--- misc/nvim-dap.lua
+++ misc/nvim-dap.lua
@@ -1,15 +1,54 @@
 local dap = require("dap")
 
+local extension_path = os.getenv("HOME") .. "/.local/bin/cpptools/extension/"
+local opendebug_path = extension_path .. "debugAdapters/bin/OpenDebugAD7"
+
+function Split(s, delimiter)
+	local result = {}
+	for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
+		table.insert(result, match)
+	end
+	return result
+end
+
+dap.adapters.cppdbg = {
+	id = "cppdbg",
+	type = "executable",
+	command = opendebug_path,
+  options = {
+    detached = false
+  }
+}
+
 dap.configurations.rust = {
-    {
-        name = "rust",
-        type = "rt_lldb",
-        request = "launch",
-        program = "${workspaceFolder}/build/host/stage1/bin/rustc",
-        cwd =  "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {"-V"},
-    },
+	{
+		name = "rust",
+		type = "rt_lldb",
+		request = "launch",
+		program = "${workspaceFolder}/build/host/stage1/bin/rustc",
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = { "-V" },
+	},
+}
+
+dap.configurations.c = {
+	{
+		name = "qemu-kernel-gdb",
+		type = "cppdbg",
+		request = "launch",
+		miDebuggerServerAddress = "127.0.0.1:1234",
+		program = "${workspaceFolder}/vmlinux",
+		cwd = "${workspaceFolder}",
+		stopAtEntry = false,
+		args = {},
+		environment = {},
+		externalConsole = true,
+		logging = {
+			engingLogging = false,
+		},
+		MIMode = "gdb",
+	},
 }
 
 -- vim.notify(vim.inspect(require("dap")))
