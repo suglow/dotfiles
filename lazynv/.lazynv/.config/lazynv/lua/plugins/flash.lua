6508f6d3719aa151c7c7cd41599a98aeac03d706
suglow006 <suglow006@gmail.com>
2024-03-26 02:48:40 +0000

    fix(keymap): use S in normal mode as flash select

--- lazynv/.lazynv/.config/lazynv/lua/plugins/flash.lua
+++ lazynv/.lazynv/.config/lazynv/lua/plugins/flash.lua
@@ -18,8 +18,9 @@ return {
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      -- { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "S", "vs", desc = "fast visual selection", remap = true},
+      { "S", mode = "n" , function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      -- { "S", "vs", desc = "fast visual selection", remap = true},
+      -- { "S", false, mode = { "n", "o", "x" }},
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
