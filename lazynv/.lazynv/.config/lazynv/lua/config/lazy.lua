46598f9b11953ea747f4733779ba75d592214106
suglow006 <suglow006@gmail.com>
2024-03-29 11:26:37 +0800

    feat(tailwind): add lsp tailwind

--- lazynv/.lazynv/.config/lazynv/lua/config/lazy.lua
+++ lazynv/.lazynv/.config/lazynv/lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     -- { import = "lazyvim.plugins.extras.lang.go" },
